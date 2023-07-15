eb0da243ccee4dcd167cacdf723ae9f105340bb4
Stella Del Mar <113214552+Stelladelmar@users.noreply.github.com>
2023-07-15 21:32:46 +0100

    London10-stelladelmar_rodriguez-baby names react
    up to name-card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import {useState} from "react"
 import './App.css';
+import {names} from "./names"
+import NameCard from"./components/NameCard";
 
 function App() {
+  const [namesList, setNamesList] =useState(names)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "App">
+      <div className ="container">
+        <div className ="name-container">
+          {namesList.map((name)=>{
+            return(
+              <NameCard key={name.id} data ={name} />
+            )
+          })
+          }
+        </div>
+      </div>
     </div>
   );
 }
