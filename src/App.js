73bc91a2af718a0e94f74f73eb2d1a8a3cb629b3
Stella Del Mar <113214552+Stelladelmar@users.noreply.github.com>
2023-08-23 19:57:27 +0100

    lvl1
    lvl1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import './App.css';
 import {useState} from "react"
 
-import {names} from "./names"
+import './App.css';
+import { names } from "./names"
+
 import NameCard from"./components/NameCard";
 
 function App() {
-  const [namesList, setNamesList] = useState (names)
+  const [namesList, setNamesList] = useState(names)
   return (
     <div className = "App">
       <div className ="container">
         <div className ="name-container">
-          {namesList.map((name)=>{
+          {namesList
+          .sort((a,b)=> a.name.localeCompare(b.name))
+          .map((name)=>{
             return(
               <NameCard key={name.id} data ={name} />
             )
--- src/components/NameCard.jsx
+++ src/components/NameCard.jsx
@@ -1,6 +1,6 @@
 const NameCard =({data}) => {
     return (
-        <div className ={`${data.sex}card`}>
+        <div className ={`${data.sex} card`}>
             <h1>{data.name}</h1>
         </div>
     );
